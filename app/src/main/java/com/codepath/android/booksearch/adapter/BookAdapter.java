051010cb661ae29ebc78dd92f725c376c80a6335
wosea <phamduyduypham@gmail.com>
2016-10-19 19:55:51 +0700

    PlaywithAdapter
    something new

--- app/src/main/java/com/codepath/android/booksearch/adapter/BookAdapter.java
+++ app/src/main/java/com/codepath/android/booksearch/adapter/BookAdapter.java
@@ -1,6 +1,7 @@
 package com.codepath.android.booksearch.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 
 import com.codepath.android.booksearch.R;
 import com.codepath.android.booksearch.model.Book;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,22 +26,34 @@ public class BookAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     public void setBooks(List<Book> books) {
         // TODO: Insert your code here
+        mBooks=books;
+        notifyDataSetChanged();
+
     }
 
     public void addBooks(List<Book> books) {
-        // int startPosition = getItemCount();
+         int startPosition = getItemCount();
         // TODO: Insert your code here
+        mBooks.addAll(books);
+        notifyItemRangeChanged(startPosition,books.size());
     }
+
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // TODO: Insert your code here
-        return null;
+        View itemview = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_book,parent,false);
+        return new ViewHolder(itemview);
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         // TODO: Insert your code here
+        Book book= mBooks.get(position);
+        ViewHolder viewholder= (ViewHolder) holder;
+        viewholder.tvTitle.setText("Tadk1");
+        Picasso.with(holder.)
+
     }
 
     @Override
